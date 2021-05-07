a97b793dd6de3ac3ed8c132105e9f73e31278090
j4ix <jai@j4ix.com>
2021-05-06 20:35:11 -0400

    1.1

--- batchexample.bat
+++ batchexample.bat
@@ -1,1 +1,19 @@
-catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix c:/users/j4ix/music Dubstep
+::pyscdl v1.1
+::
+::usage: catalogupdater.exe [-h] <cid> <url> [output_dir] [default_genre] [do_meta]
+::
+::required arguments:
+::  cid                client id
+::  url                soundcloud user by url
+::
+::optional arguments:
+::  -h, --help         show this shit
+::  output_dir         output directory
+::  default_genre      default genre to use if none
+::  do_meta            compute metadata for downloaded tracks
+::
+::examples:
+::  catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix . Dubstep True         -->  download to current directory with meta processing
+::  catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix c:/users/j4ix/music    -->  download to music directory with meta processing
+
+catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix .
