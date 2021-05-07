b7beb1dff9e750a41e92c7d7e6f2a2311427548e
j4ix <jai@j4ix.com>
2021-05-06 20:45:53 -0400

    update 1.1

--- batchexample.bat
+++ batchexample.bat
@@ -1,19 +1,19 @@
-::pyscdl v1.1
+::  pyscdl v1.1
 ::
-::usage: catalogupdater.exe [-h] <cid> <url> [output_dir] [default_genre] [do_meta]
+::  usage: catalogupdater.exe [-h] <cid> <url> [output_dir] [default_genre] [do_meta]
 ::
-::required arguments:
-::  cid                client id
-::  url                soundcloud user by url
+::  required arguments:
+::    cid                client id
+::    url                soundcloud user by url
 ::
-::optional arguments:
-::  -h, --help         show this shit
-::  output_dir         output directory
-::  default_genre      default genre to use if none
-::  do_meta            compute metadata for downloaded tracks
+::  optional arguments:
+::    -h, --help         show this shit
+::    output_dir         output directory
+::    default_genre      default genre to use if none
+::    do_meta            compute metadata for downloaded tracks
 ::
-::examples:
-::  catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix . Dubstep True         -->  download to current directory with meta processing
-::  catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix c:/users/j4ix/music    -->  download to music directory with meta processing
+::  examples:
+::    catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix . Dubstep True         -->  download to current directory with meta processing
+::    catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix c:/users/j4ix/music    -->  download to music directory with meta processing
 
 catalogupdater.exe Mx2TehYCr804EIo6LQ7OARjZpWjMdVOx j4ix .
